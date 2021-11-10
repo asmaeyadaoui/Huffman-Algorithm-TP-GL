d1d42b335951155de7d22b9cf1acd2b79cf71133
ZynoZin <zineddinelamziouik@gmail.com>
2021-11-10 23:56:11 +0100

    Added the printCode function

--- Huffman.java
+++ Huffman.java
@@ -16,3 +16,29 @@ class MyComparator implements Comparator<HuffmanNode> {
 	}
 }
  
+public class Huffman {
+ 
+    public static void printCode(HuffmanNode root, String s)
+    {
+ 
+        
+        if (root.left
+                == null
+            && root.right
+                   == null
+            && Character.isLetter(root.c)) {
+ 
+            System.out.println(root.c + ":" + s);
+ 
+            return;
+        }
+ 
+        
+        printCode(root.left, s + "0");
+        printCode(root.right, s + "1");
+    }
+     public static void main(String[] args)
+    {
+    }
+}
+
